0e091b17bc5a2478a64e1cd383367d791e020b48
thegreatone9 <musashakib123@gmail.com>
2020-06-10 22:36:36 +0600

    headers and footers

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Card, CardImg, CardTitle, CardBody, CardText } from 'reactstrap';
 
-function RenderDish({dish}){
+function RenderDish({dishID}){
     //console.log(props.dishID);
     return(
         <Card>
-            <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
+            <CardImg width="100%" src={dishID.image} alt={dishID.name}></CardImg>
             <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
+                <CardTitle>{dishID.name}</CardTitle>
+                <CardText>{dishID.description}</CardText>
             </CardBody>
         </Card>
     );
@@ -40,7 +40,7 @@ const DishDetail = (props) => {
                     </div>
                     <div className = "col-12 col-md-5 m-1">
                         <h4>Comments</h4>
-                        <RenderComments dishID = {props.dishID.comments}/>
+                        <RenderComments comments = {props.dishID.comments}/>
                     </div>
                 </div>
             </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import { Navbar, NavbarBrand} from 'reactstrap';
 import { DISHES } from '../shared/dishes.js';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
 
@@ -17,18 +19,16 @@ class Main extends Component {
   //method updating the state lies in the main component
   onDishSelect(dishID){
     this.setState({ selectedDish: dishID });
+    console.log(this.selectedDish);
   }
 
   render() {
     return (
       <div className="">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
         <Menu dishes = {this.state.dishes} onClick = {(dishID) => this.onDishSelect(dishID)} />
         <DishDetail dishID = {this.state.dishes.filter((dish) => this.state.selectedDish === dish.id)[0]}/>
+        <Footer />
       </div>
     );
   }

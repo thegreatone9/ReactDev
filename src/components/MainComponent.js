6b2da7e58eb34e1bd58a1faa2e186f24aabd8db0
thegreatone9 <musashakib123@gmail.com>
2020-06-11 13:02:31 +0600

    SPA part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import { Navbar, NavbarBrand} from 'reactstrap';
 import { DISHES } from '../shared/dishes.js';
+import { COMMENTS } from '../shared/comments.js';
+import { LEADERS } from '../shared/leaders.js';
+import { PROMOTIONS } from '../shared/promotions.js';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
 
 //The main component is responsible for everything related to state and data flow throughout the components
@@ -14,14 +18,21 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      leaders: LEADERS,
+      promotions: PROMOTIONS
     };
   }
 
 
   render() {
+    //filter returns an array...since, we know only 1 item is in the array, we just selected the first and only item using [0]
+    console.log(this.state.dishes.filter((dish) => dish.featured)[0]);  
     const HomePage = () => {
         return(
-            <Home />
+            <Home dish = {this.state.dishes.filter((dish) => dish.featured)[0]} 
+                  promotions = {this.state.promotions.filter((promo) => promo.featured)[0]}
+                  leaders = {this.state.leaders.filter((leader) => leader.featured)[0]}  />
         );
     }
 
@@ -33,6 +44,7 @@ class Main extends Component {
             <Route path="/home" component={HomePage} />
             {/* to pass props to component inside Route, you write it like this: */}
             <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
+            <Route exact path="/contactus" component={Contact} />
             {/*anything doesn't match home or menu will be redirected to the default of home: */}
             <Redirect to="/home" />
         </Switch>
